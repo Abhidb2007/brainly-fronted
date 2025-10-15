9cc73b31f333e3b698ed0e80891d04fe14ad9c4f
Abhidb2007 <abhishekdb2210@gmail.com>
2025-10-15 22:22:22 +0530

    code

--- src/components/icon/PlusIcon.tsx
+++ src/components/icon/PlusIcon.tsx
@@ -1,7 +1,8 @@
 export const PlusIcon=(props: IconProps)=>{
     return <svg xmlns="http://www.w3.org/200/svg" fill="none"
-    viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+    className={iconSizeVariants[props.size]}>
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 
     </svg>
 }
