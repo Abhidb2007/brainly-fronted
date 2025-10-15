6cb0225b7bd6e64fc936ab991ce4cabd44a03137
Abhidb2007 <abhishekdb2210@gmail.com>
2025-10-15 19:17:42 +0530

    buttton code

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App(){
   return(
     <>
     <Button size="sm" variant="primary" text="share"/>
-    <button size="md" variant="secondary" text="Add content"/>
-    <button size="lg" variant="primary" text="Login"/>
+    <Button size="md" variant="secondary" text="Add content"/>
+    <Button size="lg" variant="primary" text="Login"/>
     </>
   )
 }
